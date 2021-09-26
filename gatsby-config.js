94dee71951618634759cc321495445a7eec9e670
Yan Paing Oo <yanpaingoo008ypo@gmail.com>
2021-09-26 19:05:04 +0630

    Change gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@YPO_007',
   },
-  pathPrefix: "/ypo777.github.io",
 
   plugins: [
     `gatsby-plugin-react-helmet`,
