9cd040f364d8e73d63847980d4801aa8060d5ca2
Yan Paing Oo <yanpaingoo008ypo@gmail.com>
2022-04-16 19:40:20 +0630

    Change Thumbnail Image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Yan Paing Oo is a student currently attending in UIT(Yangon) Knowledge Engineering Major',
     siteUrl: 'https://ypo777.github.io', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/og_new.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@YPO_007',
   },
 
